5c2d9eb37f221bf8c9ce612d03bf0c26a490e6d0
Szabolcs Szakacs <szsza88@gmail.com>
2019-06-08 15:16:33 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insert password: ");
+
+        String pwd = scanner.nextLine();
+        boolean valid = true;
+
+        if (pwd.length()<6) {
+            System.out.println("Parola trebuie sa aiba cel putin 6 caractere");
+            valid = false;
+        }
+        if (!Pattern.matches(".*[a-z].*", pwd)) {
+            System.out.println("Parola trebuie sa aiba cel putin o litera mica");
+            valid=false;
+        }
+        if (!Pattern.matches(".*[A-Z].*", pwd)) {
+            System.out.println("Parola trebuie sa aiba cel putin o litera mare");
+            valid=false;
+        }
+        if (!Pattern.matches(".*\\d.*", pwd)) {
+            System.out.println("Parola trebuie sa aiba cel putin o cifra");
+            valid=false;
+        }
+
+        System.out.println(valid ? "Parola a fost acceptata" : "Parola incorecta. Incercati din nou.");
+
+
+        /*
+        for (String a:pwd){
+            if (Pattern.matches("^(?=.{10,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$", a) && pwd.length<=6)
+                System.out.println("OK");
+        } else System.out.println("Not OK");
+         */
+
+
+    }
+}
